129246f2beb69aec700f15ecf892502676207d14
James Gooding <38703245+jameseg@users.noreply.github.com>
2024-07-03 14:45:56 -0400

    Get button working after second click
    need to explicitly call class to add class

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ const button = document.querySelector("button")
 
 function drawGrid(parent, rowItems = 16) {
   const squareSidePercentage = (100 / rowItems)
-  console.log(squareSidePercentage + '%')
+
   for (let i = 0; i < rowItems * rowItems; i++) {
     const box = document.createElement("div")
 
     box.addEventListener("mouseover", () => {
       box.style.backgroundColor = "gray"
     })
+
     box.classList.add("box")
     box.style.flex = `1 1 calc(${squareSidePercentage}% - 2px)`
+
     parent.appendChild(box)
   }
 }
@@ -22,7 +24,8 @@ function removeGrid() {
 }
 
 function createNewContainer(input) {
-  const newContainer = document.createElement("div.boxes-container")
+  const newContainer = document.createElement("div")
+  newContainer.classList.add("boxes-container")
   newContainer.style.display = 'flex';
   newContainer.style.justifyContent = 'center';
   newContainer.style.flexWrap = 'wrap';
@@ -30,21 +33,27 @@ function createNewContainer(input) {
   newContainer.style.width = '960px'
   newContainer.style.height = '960px'
   newContainer.style.flex = 1;
+
   const body = document.querySelector("body")
   body.appendChild(newContainer)
+  console.log(newContainer)
 
   drawGrid(newContainer, input)
 }
 
 button.addEventListener("click", () => {
   const input = +prompt("How many squares per side? (limit 100)")
-  console.log(input)
+  const parentContainer = document.querySelector(".boxes-container")
+  parentContainer.remove(".boxes-container")
 
-  removeGrid()
   createNewContainer(input)
 })
 
+// start the program
 drawGrid(container)
+
+
+
 
 
 
