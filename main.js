2b6de55316acb6e55af4d0dda7ae3c2d3310ac73
James Gooding <38703245+jameseg@users.noreply.github.com>
2024-07-03 12:27:47 -0400

    Get replacement grid working on first click
    container needs new styles

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const container = document.querySelector(".boxes-container")
 const button = document.querySelector("button")
 
-function drawGrid(rowItems = 16) {
+function drawGrid(parent, rowItems = 16) {
   const squareSidePercentage = (100 / rowItems)
-  console.log(squareSidePercentage)
+  console.log(squareSidePercentage + '%')
   for (let i = 0; i < rowItems * rowItems; i++) {
     const box = document.createElement("div")
 
@@ -11,36 +11,40 @@ function drawGrid(rowItems = 16) {
       box.style.backgroundColor = "gray"
     })
     box.classList.add("box")
-    // box.textContent = i + 1
-    // box.style.width = squareSide
-    // box.style.height = squareSide
     box.style.flex = `1 1 calc(${squareSidePercentage}% - 2px)`
-    container.appendChild(box)
+    parent.appendChild(box)
   }
 }
 
-// button.addEventListener("click", (e) => {
-//   const input = +prompt("How many squares per side? (limit 100)")
-//   console.log(input)
+function removeGrid() {
+  const parentContainer = document.querySelector(".boxes-container")
+  parentContainer.remove(".boxes-container")
+}
 
-//   container.remove(".boxes-container")
+function createNewContainer(input) {
+  const newContainer = document.createElement("div.boxes-container")
+  newContainer.style.display = 'flex';
+  newContainer.style.justifyContent = 'center';
+  newContainer.style.flexWrap = 'wrap';
+  newContainer.style.margin = 'auto';
+  newContainer.style.width = '960px'
+  newContainer.style.height = '960px'
+  newContainer.style.flex = 1;
+  const body = document.querySelector("body")
+  body.appendChild(newContainer)
 
-//   const container = document.createElement("div.boxes-container")
-//   for (let i = 0; i < input; i++) {
-//     const box = document.createElement("div")
+  drawGrid(newContainer, input)
+}
 
-//     box.addEventListener("mouseover", (e) => {
-//       box.style.backgroundColor = "gray"
-//     })
+button.addEventListener("click", () => {
+  const input = +prompt("How many squares per side? (limit 100)")
+  console.log(input)
 
-//     box.classList.add("box")
+  removeGrid()
+  createNewContainer(input)
+})
 
-
-//     container.appendChild(box)
-//   }
-// })
-
-drawGrid(100)
+drawGrid(container)
 
 
 
