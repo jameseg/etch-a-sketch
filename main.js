dfe491d8259585d42efbde8e587939477c19d955
James Gooding <38703245+jameseg@users.noreply.github.com>
2024-07-03 11:49:31 -0400

    Get dynamic grid working

--- main.js
+++ main.js
@@ -1,12 +1,47 @@
 const container = document.querySelector(".boxes-container")
+const button = document.querySelector("button")
 
-for (let i = 1; i <= 256; i++) {
-  const box = document.createElement("div")
+function drawGrid(rowItems = 16) {
+  const squareSidePercentage = (100 / rowItems)
+  console.log(squareSidePercentage)
+  for (let i = 0; i < rowItems * rowItems; i++) {
+    const box = document.createElement("div")
 
-
-  box.textContent = i
-  box.classList.add("box")
-
-  container.appendChild(box)
+    box.addEventListener("mouseover", () => {
+      box.style.backgroundColor = "gray"
+    })
+    box.classList.add("box")
+    // box.textContent = i + 1
+    // box.style.width = squareSide
+    // box.style.height = squareSide
+    box.style.flex = `1 1 calc(${squareSidePercentage}% - 2px)`
+    container.appendChild(box)
+  }
 }
+
+// button.addEventListener("click", (e) => {
+//   const input = +prompt("How many squares per side? (limit 100)")
+//   console.log(input)
+
+//   container.remove(".boxes-container")
+
+//   const container = document.createElement("div.boxes-container")
+//   for (let i = 0; i < input; i++) {
+//     const box = document.createElement("div")
+
+//     box.addEventListener("mouseover", (e) => {
+//       box.style.backgroundColor = "gray"
+//     })
+
+//     box.classList.add("box")
+
+
+//     container.appendChild(box)
+//   }
+// })
+
+drawGrid(100)
+
+
+
 
